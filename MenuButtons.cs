c4173d540b05ea80394e73735981e57d7a7bada5
SarantaS <65202305+SarantaS@users.noreply.github.com>
2020-05-15 22:42:42 +0100

    Added MenuButton script that activates the buttons
    Already informed supervisor of code in the report since submission but adding it now just in case.

---
+++ MenuButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void RestartSim()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ExitSim()
+    {
+        Application.Quit();
+    }
+    public void Travel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "House")
+        {
+            SceneManager.LoadScene("School");
+        }
+        else
+        {
+            SceneManager.LoadScene("House");
+        }
+    }
+}
+
