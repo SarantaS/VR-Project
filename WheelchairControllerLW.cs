4574b9c3837b1fbfe7e4d7291f6dc89d2a493385
SarantaS <65202305+SarantaS@users.noreply.github.com>
2020-05-15 22:46:34 +0100

    Added wheelchair scripts
    Already informed supervisor of code in the report since submission but adding it now just in case.

---
+++ WheelchairControllerLW.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+
+public class WheelchairControllerLW : MonoBehaviour
+{
+    public VRTK_BaseControllable controllable;
+    public GameObject wheelchair;
+    public GameObject pivotPoint;
+    public float speed;
+    private float nr;
+    private float or;
+    private int direction = 0;
+
+    private void Update()
+    {
+        if (or < nr)
+        {
+            direction = 1;
+        }
+        if (or > nr)
+        {
+            direction = -1;
+        }
+        or = nr;
+    }
+    private void LateUpdate()
+    {
+        nr = controllable.GetValue();
+    }
+
+    [System.Obsolete]
+    protected virtual void OnEnable()
+    {
+        if (controllable != null)
+        {
+            controllable.ValueChanged += ValueChanged;
+        }
+    }
+
+    [System.Obsolete]
+    protected virtual void OnDisable()
+    {
+        if (controllable != null)
+        {
+            controllable.ValueChanged -= ValueChanged;
+        }
+    }
+
+    [System.Obsolete]
+    protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+    {
+        if (wheelchair != null)
+        {
+            if (direction == 1)
+            {
+                wheelchair.transform.RotateAround(pivotPoint.transform.position, Vector3.up, (speed * e.normalizedValue * Time.deltaTime));
+                wheelchair.transform.Translate(Vector3.forward * (e.normalizedValue * Time.deltaTime));
+            }
+            else if (direction == -1)
+            {
+                wheelchair.transform.RotateAround(pivotPoint.transform.position, Vector3.up, -(speed * e.normalizedValue * Time.deltaTime));
+                wheelchair.transform.Translate(Vector3.forward * -(e.normalizedValue * Time.deltaTime));
+            }
+        }
+    }
+}
+
